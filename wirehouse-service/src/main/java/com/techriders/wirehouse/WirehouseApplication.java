e7304283d6d3fd51c181109ce0b1261d04d073c4
sbr mgr <sbr.mgr1@gmail.com>
2020-08-10 18:17:43 -0500

    Manages services

---
+++ wirehouse-service/src/main/java/com/techriders/wirehouse/WirehouseApplication.java
@@ -0,0 +1,13 @@
+package com.techriders.wirehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WirehouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WirehouseApplication.class, args);
+    }
+
+}
---
+++ wirehouse-service/src/test/java/com/techriders/wirehouse/WirehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techriders.wirehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WirehouseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
