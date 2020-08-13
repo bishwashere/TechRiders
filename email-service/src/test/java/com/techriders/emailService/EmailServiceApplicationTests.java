f692e60baa207e38612c242fbfab2ac6a85e11cd
sbr mgr <sbr.mgr1@gmail.com>
2020-08-13 08:26:07 -0500

    Final Managed

--- email-service/src/test/java/com/techriders/emailService/EmailServiceApplicationTests.java
+++ email-service/src/test/java/com/techriders/emailService/EmailServiceApplicationTests.java
@@ -1,19 +1,9 @@
-<<<<<<< HEAD:warehouseService/src/test/java/com/techriders/logisticservice/LogisticserviceApplicationTests.java
-package com.techriders.logisticservice;
-=======
 package com.techriders.emailService;
->>>>>>> 74e49a5e86fbe11f21dd98e2f840978f217ebe7a:email-service/src/test/java/com/techriders/emailService/EmailServiceApplicationTests.java
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-<<<<<<< HEAD:warehouseService/src/test/java/com/techriders/logisticservice/LogisticserviceApplicationTests.java
-class LogisticserviceApplicationTests {
-=======
 class EmailServiceApplicationTests {
->>>>>>> 74e49a5e86fbe11f21dd98e2f840978f217ebe7a:email-service/src/test/java/com/techriders/emailService/EmailServiceApplicationTests.java
-
     @Test
     void contextLoads() {
     }
