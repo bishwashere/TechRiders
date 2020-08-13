8b5bbbe4409edd9381f351e5e32ad3c3e3324fb4
sbr mgr <sbr.mgr1@gmail.com>
2020-08-13 01:52:23 -0500

    Permission to Content Management

--- front-service/src/main/java/com/techriders/frontservice/services/ContentService.java
+++ front-service/src/main/java/com/techriders/frontservice/services/ContentService.java
@@ -2,13 +2,18 @@ package com.techriders.frontservice.services;
 
 
 import com.techriders.frontservice.domains.Content;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 import java.util.List;
 
 public interface ContentService {
     public List<Content> getAllContents();
+
+
     public Content save(Content content);
+
     public Content find(String slug);
-    //Content editContentBySlug(String slug);
+
+
     public void delete(String slug);
 }
--- warehouseService/src/main/java/com/warehouseService/rabbitmq/services/ContentService.java
+++ warehouseService/src/main/java/com/warehouseService/rabbitmq/services/ContentService.java
@@ -2,13 +2,17 @@ package com.warehouseService.rabbitmq.services;
 
 
 import com.warehouseService.rabbitmq.domains.Content;
+import org.springframework.security.access.prepost.PreAuthorize;
 
 import java.util.List;
 
 public interface ContentService {
     public List<Content> getAllContents();
+
+    @PreAuthorize("hasPermission(#content,'content-management')")
     public Content save(Content content);
     public Content find(String slug);
-    //Content editContentBySlug(String slug);
+
+    @PreAuthorize("hasPermission(#content,'content-management')")
     public void delete(String slug);
 }
